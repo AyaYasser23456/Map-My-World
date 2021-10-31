4e853c274cf57604bb723acbc9c4989660c1bc73
Aya Yasser <Ayaabotaleb25@gamil.com>
2021-11-01 01:11:02 +0200

    first

---
+++ catkin_ws/build/RTABmap/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws/src/RTABmap
+# Build directory: /home/workspace/catkin_ws/build/RTABmap
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/aya_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws/src/aya_pkg
+# Build directory: /home/workspace/catkin_ws/build/aya_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
